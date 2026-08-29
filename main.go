532550669b08bf32b8c6695bdfe7d05eedfb853d
daengdaengLee <gunho1020@gmail.com>
2026-08-29 11:19:39 +0900

    fix: cobra 커맨드의 자동 에러 출력을 비활성화하고 main 함수에서 명시적으로 로깅
    cobra 의 자동 에러 출력(SilenceErrors)은 stderr 를 사용하고 출력 규칙도
    cobra 내부 규칙을 따르므로, 로깅 방식과 규칙을 통일하기 위해 비활성화했다.
    대신 main 의 Execute() 반환 error 를 기본 slog 로 직접 로깅하도록 바꿨다.
    
    close #7

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"log/slog"
 	"os"
 	"recon/cmd"
 )
 
 func main() {
 	if err := cmd.NewRootCmd().Execute(); err != nil {
-		// cobra가 Execute() 내부에서 이미 에러를 stderr에 출력하므로
-		// 여기서는 비정상 종료 코드만 반환한다.
+		slog.Error("root 커맨드 에러", slog.Any("error", err))
 		os.Exit(1)
 	}
 }
